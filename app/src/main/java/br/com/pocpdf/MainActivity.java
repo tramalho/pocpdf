73b92004d7c770e5996490fffef09721b6426ac6
trama <thiagofacul@gmail.com>
2017-08-30 00:43:55 -0300

    open pdf driver viewer

---
+++ app/src/main/java/br/com/pocpdf/MainActivity.java
@@ -0,0 +1,51 @@
+package br.com.pocpdf;
+
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
+
+public class MainActivity extends AppCompatActivity {
+
+    private String pdf = "http://www.adobe.com/devnet/acrobat/pdfs/pdf_open_parameters.pdf";
+
+    private WebView webview;
+    private ProgressBar progressBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initView();
+    }
+
+    private void initView() {
+        progressBar = (ProgressBar) findViewById(R.id.progress_bar);
+        webview = (WebView) findViewById(R.id.webview);
+        webview.getSettings().setJavaScriptEnabled(true);
+        webview.setWebViewClient(new WebViewClient() {
+            @Override
+            public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                super.onPageStarted(view, url, favicon);
+                webview.setVisibility(View.GONE);
+                progressBar.setVisibility(View.VISIBLE);
+            }
+
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
+                progressBar.setVisibility(View.GONE);
+                webview.setVisibility(View.VISIBLE);
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        webview.loadUrl("http://drive.google.com/viewerng/viewer?embedded=true&url=" + pdf);
+    }
+}
