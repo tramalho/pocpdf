98a060301dea1245f1ce251881cadefa1dedbc8a
tramalho <tramalho@ciandt.com>
2017-09-20 19:21:40 -0300

    download with rx

--- app/src/main/java/br/com/pocpdf/MainActivity.java
+++ app/src/main/java/br/com/pocpdf/MainActivity.java
@@ -1,16 +1,52 @@
 package br.com.pocpdf;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.ProgressBar;
 
+import java.io.File;
+import java.io.IOException;
+
+import okhttp3.OkHttpClient;
+import okhttp3.ResponseBody;
+import okhttp3.logging.HttpLoggingInterceptor;
+import okio.BufferedSink;
+import okio.Okio;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory;
+import rx.Observable;
+import rx.Observer;
+import rx.Subscriber;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.functions.Func1;
+import rx.schedulers.Schedulers;
+
 public class MainActivity extends AppCompatActivity {
 
-    private String pdf = "http://www.adobe.com/devnet/acrobat/pdfs/pdf_open_parameters.pdf";
+    private static final String TAG = MainActivity.class.getClass().getSimpleName();
+    private String pdf_base = "http://www.adobe.com/devnet/acrobat/pdfs/";
+    private String pdf_file = "pdf_open_parameters.pdf";
+
+    // Storage Permissions
+    private static final int REQUEST_EXTERNAL_STORAGE = 1;
+    private static String[] PERMISSIONS_STORAGE = {
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+    };
 
     private WebView webview;
     private ProgressBar progressBar;
@@ -20,6 +56,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initView();
+
+        verifyStoragePermissions(this);
     }
 
     private void initView() {
@@ -43,9 +81,128 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-        webview.loadUrl("http://drive.google.com/viewerng/viewer?embedded=true&url=" + pdf);
+    public void webviewAction(View view) {
+        webview.loadUrl("http://drive.google.com/viewerng/viewer?embedded=true&url=" +
+                pdf_base.concat(pdf_file));
+    }
+
+    private <T> T createService(Class<T> serviceClass, String baseUrl) {
+
+        HttpLoggingInterceptor logging = new HttpLoggingInterceptor();
+        // set your desired log level
+        logging.setLevel(HttpLoggingInterceptor.Level.BODY);
+        OkHttpClient.Builder httpClient = new OkHttpClient.Builder();
+        httpClient.addInterceptor(logging);
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(baseUrl)
+                .client(httpClient.build())
+                .addCallAdapterFactory(RxJavaCallAdapterFactory.create()).build();
+        return retrofit.create(serviceClass);
+    }
+
+    private void verifyStoragePermissions(Activity activity) {
+        // Check if we have read or write permission
+        int writePermission = ActivityCompat.checkSelfPermission(activity, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        int readPermission = ActivityCompat.checkSelfPermission(activity, Manifest.permission.READ_EXTERNAL_STORAGE);
+
+        if (writePermission != PackageManager.PERMISSION_GRANTED || readPermission != PackageManager.PERMISSION_GRANTED) {
+            // We don't have permission so prompt the user
+            ActivityCompat.requestPermissions(
+                    activity,
+                    PERMISSIONS_STORAGE,
+                    REQUEST_EXTERNAL_STORAGE
+            );
+        }
+    }
+
+    public void shareAction(View view) {
+
+
+
+        progressBar.setVisibility(View.VISIBLE);
+        RetrofitInterface downloadService = createService(RetrofitInterface.class, pdf_base);
+        downloadService.downloadFileByUrlRx(pdf_file)
+                .flatMap(processResponse())
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(handleResult());
+    }
+
+    private Func1<Response<ResponseBody>, Observable<File>> processResponse() {
+        return new Func1<Response<ResponseBody>, Observable<File>>() {
+            @Override
+            public Observable<File> call(Response<ResponseBody> responseBodyResponse) {
+                return saveToDiskRx(responseBodyResponse);
+            }
+        };
+    }
+
+    private Observable<File> saveToDiskRx(final Response<ResponseBody> responseBodyResponse) {
+        return Observable.create(new Observable.OnSubscribe<File>() {
+            @Override
+            public void call(Subscriber<? super File> subscriber) {
+                BufferedSink bufferedSink = null;
+                try {
+                    String fileName = "pdf.pdf";
+                    // will create file in global Music directory, can be any other directory, just don't forget to handle permissions
+
+                    File file = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS).getAbsoluteFile(), fileName);
+
+                    bufferedSink = Okio.buffer(Okio.sink(file));
+                    // you can access body of response
+                    bufferedSink.writeAll(responseBodyResponse.body().source());
+                    bufferedSink.close();
+
+                    subscriber.onNext(file);
+                    subscriber.onCompleted();
+                } catch (IOException e) {
+                    subscriber.onError(e);
+                } finally {
+                    if (bufferedSink != null) {
+                        try {
+                            bufferedSink.close();
+                        } catch (IOException e) {
+                            Log.d(TAG, "bufferedSink.close() ", e);
+                        }
+                    }
+                }
+            }
+        });
+    }
+
+    private Observer<File> handleResult() {
+        return new Observer<File>() {
+            @Override
+            public void onCompleted() {
+                progressBar.setVisibility(View.GONE);
+                Log.d(TAG, "onCompleted");
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                progressBar.setVisibility(View.GONE);
+                Log.d(TAG, "Error " + e.getMessage());
+            }
+
+            @Override
+            public void onNext(File file) {
+                openPdf(file);
+                Log.d(TAG, "File downloaded to " + file.getAbsolutePath());
+            }
+        };
+    }
+
+    private void openPdf(File file) {
+        Intent target = new Intent(Intent.ACTION_VIEW);
+        target.setDataAndType(Uri.fromFile(file),"application/pdf");
+        target.setFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
+
+        Intent intent = Intent.createChooser(target, "Open File");
+        try {
+            startActivity(intent);
+        } catch (ActivityNotFoundException e) {
+            // Instruct the user to install a PDF reader here, or something
+        }
     }
 }
